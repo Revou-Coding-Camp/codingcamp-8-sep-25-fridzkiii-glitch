106f02ce24fc269b043934fdcb49a5f42f7621cf
fridzkiii-glitch <fridzkiii@gmail.com>
2025-09-12 21:53:59 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+welcomeMessage();
+function welcomeMessage() {
+    let username = prompt("Please enter your name:");
+    if (username) {
+        document.getElementById('welcome-user').innerText = username;
+    }
+}
+
+function validateForm() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("All fields must be filled out");
+    } else {
+        alert("Form submitted successfully! Thank you, " + name + "!");
+        document.getElementById('name').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('message').value = "";
+    }
+}
